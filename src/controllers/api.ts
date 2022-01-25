25a4a85f297e56482a9176631b77ff9e46664ac9
jnavarro <21sebas12@gmail.com>
2022-01-24 22:00:44 -0500

    end back-end to crawler api

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,11 +1,15 @@
 import { Application, Request, Response } from 'express';
-import { crawl } from '../services';
+import { crawler } from '../services';
 
 export const loadApiEndpoints = (app: Application): void => {
   app.get('/', (req: Request, res: Response) => {
-    return res.status(200).send("It's runing");
+    return res
+      .status(200)
+      .send("It's runing for check all the info just '/crawler'");
   });
-  app.get('/test', (req: Request, res: Response) => {
-    return res.status(200).send(crawl('https://www.lightningreach.org/'));
+  app.get('/crawler', async (req: Request, res: Response) => {
+    const responst = await crawler('https://www.lightningreach.org/');
+
+    return res.status(200).json(responst);
   });
 };
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,10 +1,6 @@
 import fetch from 'node-fetch';
 import * as cheerio from 'cheerio';
 
-export const test = (): string => {
-  return 'test';
-};
-
 // 1.1. Check if is subdomain
 const checkIsSubdomain = ({ url, mainURL }) => {
   if (url.indexOf(mainURL) !== -1) {
@@ -43,20 +39,85 @@ const getPageDomains = async ({ url, mainURL, linksReady }) => {
       return tag.attribs.href;
     })
     .get();
-
   for (let i = 0; i < links.length; i++) {
-    await getPageDomains({ url: links[i], mainURL, linksReady });
+    await getPageDomains({
+      url: links[i],
+      mainURL,
+      linksReady,
+    });
   }
 
   return linksReady;
 };
 
-export const crawl = async (url: string) => {
-  // const crawler: Array;
+// 2.1. obtain texts titles scraping
+const getTexts = (element) => {
+  const extractTextOne = element.split('">');
+  const extractTextTwo = extractTextOne[extractTextOne.length - 1].split('<');
+  return extractTextTwo[0];
+};
+
+// 2. Secont Step obtain all the information per subdomain. implement scraping
+const getInfoSubdomains = async ({ allSubdomains }) => {
+
+  const response = allSubdomains.map(async (url) => {
+    const response = await fetch(url);
+    const html = await response.text();
+    const $ = cheerio.load(html);
+
+    const links = $('a')
+      .map((i, tag) => {
+        return tag.attribs.href;
+      })
+      .get();
+    const subdomains = links.filter((x, i) => links.indexOf(x) === i);
+
+    const imagesLink = $('img')
+      .map((i, tag) => {
+        return tag.attribs.src;
+      })
+      .get();
+    const images = imagesLink.filter((x, i) => imagesLink.indexOf(x) === i);
+    let titles = [];
+    try {
+      titles = $('h1, h2, h3, h4, h5, h6')
+        .map((_, element) => {
+          const text = getTexts($(element).html());
+          return {
+            type: element.name,
+            text,
+          };
+        })
+        .get();
+    } catch (e) {
+      console.log(e);
+    }
+
+    return {
+      [`${url}`]: {
+        total_subdomains: links.length,
+        subdomains,
+        total_images: imagesLink.length,
+        images,
+        total_titles: titles.length,
+        titles,
+      },
+    };
+  });
+
+  return Promise.all(response);
+};
+
+export const crawler = async (url: string) => {
   const linksReady = [];
   const mainURL = url;
-  const subDomains = await getPageDomains({ url, mainURL, linksReady });
-  console.log('subDomains>>>', subDomains);
+  // 1. stept one
+  const allSubdomains = await getPageDomains({
+    url,
+    mainURL,
+    linksReady,
+  });
+  const infoSubdomains = await getInfoSubdomains({ allSubdomains });
 
-  // const subDomains = getSubdomains(links, url);
+  return infoSubdomains;
 };
